9dcaff5b963b1572f15a151d8f514aa7f3a4816d
Félix Giffard <technonerdzfg@gmail.com>
2017-02-02 13:55:13 -0500

    Change GA

--- js/scripts.js
+++ js/scripts.js
@@ -33,11 +33,11 @@ for (i = 0; i < links.length; i++) {
 }
 
 if (location.protocol === 'http:' || location.protocol === 'https:') {
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-    ga('create', 'UA-82626035-1', 'auto');
-    ga('send', 'pageview');
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+	
+  ga('create', 'UA-63194329-3', 'auto');
+  ga('send', 'pageview');
 }
