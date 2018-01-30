1c6440e9ef1b80cdccc4c779f61603cc967003b1
Félix Giffard <technonerdzfg@gmail.com>
2018-01-30 13:17:07 -0500

    remove old ga

--- js/scripts.js
+++ js/scripts.js
@@ -32,12 +32,3 @@ for (i = 0; i < links.length; i++) {
 	links[i].href = links[i].href.replace(/ATSYMBOL/, '@').replace(/PERIOD/, '.');
 }
 
-if (location.protocol === 'http:' || location.protocol === 'https:') {
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-	
-  ga('create', 'UA-63194329-3', 'auto');
-  ga('send', 'pageview');
-}
